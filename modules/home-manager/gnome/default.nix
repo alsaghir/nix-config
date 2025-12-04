f5b47110bee2b92de108026155af60a5354fb865
alsaghir <ahmedalsaghirxyz@gmail.com>
2025-12-04 11:53:15 +0200

    refactor

--- modules/home-manager/gnome/default.nix
+++ modules/home-manager/gnome/default.nix
@@ -9,95 +9,103 @@ let
   isGnome = osConfig.services.desktopManager.gnome.enable or false;
 in
 {
-  programs.gnome-shell = lib.mkIf isGnome {
-    enable = true;
-    extensions = with pkgs.gnomeExtensions; [
-      { package = appindicator; }
-      { package = battery-health-charging; }
-      { package = bluetooth-quick-connect; }
-      { package = blur-my-shell; }
-      { package = caffeine; }
-      { package = clipboard-indicator; }
-      { package = ddterm; }
-      { package = gnome-40-ui-improvements; }
-      { package = gpu-supergfxctl-switch; }
-      { package = gsconnect; }
-      { package = light-style; }
-      { package = native-window-placement; }
-      { package = places-status-indicator; }
-      { package = removable-drive-menu; }
-      { package = system-monitor; }
-      { package = transparent-window-moving; }
-      { package = user-themes; }
-      { package = workspace-indicator; }
-      { package = wallpaper-slideshow; }
-      { package = paperwm; }
-      { package = tiling-shell; }
-    ];
-  };
-
-  # Wayland, X, etc. support for session vars
-  systemd.user.sessionVariables = config.home.sessionVariables;
-
-  # Gnome configurations using home manager
-  gtk.theme = lib.mkIf isGnome {
-    name = "Adwaita-dark";
-    package = pkgs.gnome.gnome-themes-extra;
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
+  config = lib.mkIf isGnome {
+    programs.gnome-shell = {
+      enable = true;
+      extensions = with pkgs.gnomeExtensions; [
+        { package = appindicator; }
+        { package = battery-health-charging; }
+        { package = bluetooth-quick-connect; }
+        { package = blur-my-shell; }
+        { package = caffeine; }
+        { package = clipboard-indicator; }
+        { package = ddterm; }
+        { package = gnome-40-ui-improvements; }
+        { package = gpu-supergfxctl-switch; }
+        { package = gsconnect; }
+        { package = light-style; }
+        { package = native-window-placement; }
+        { package = places-status-indicator; }
+        { package = removable-drive-menu; }
+        { package = system-monitor; }
+        { package = transparent-window-moving; }
+        { package = user-themes; }
+        { package = workspace-indicator; }
+        { package = wallpaper-slideshow; }
+        { package = paperwm; }
+        { package = tiling-shell; }
+      ];
     };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-  };
 
-  xdg.mimeApps = lib.mkIf isGnome {
-    enable = true;
-    defaultApplications = {
-      "inode/directory" = "nemo.desktop";
-      "application/x-gnome-saved-search" = [ "nemo.desktop" ];
-    };
-  };
-  xdg.desktopEntries.nemo = lib.mkIf isGnome {
-    name = "Nemo";
-    exec = "env GTK_THEME=Adwaita:dark ${pkgs.nemo-with-extensions}/bin/nemo %U";
-    mimeType = [ "inode/directory" "application/x-gnome-saved-search" ];
-    icon = "nemo";
-  };
-  xdg.desktopEntries."org.kde.kcalc" = lib.mkIf isGnome {
-    name = "KCalc";
-    exec = "env QT_STYLE_OVERRIDE=adwaita kcalc"; 
-    icon = "kcalc";
-    comment = "Scientific Calculator";
-    categories = [ "Utility" "Calculator" ];
-  };
+    # Wayland, X, etc. support for session vars
+    systemd.user.sessionVariables = config.home.sessionVariables;
 
-  # We can now remove the hardcoded 'enabled-extensions' list
-  dconf = lib.mkIf isGnome {
-    enable = true;
-    settings = {
-      "system/locale" = {
-        region = osConfig.i18n.defaultLocale or "en_GB.UTF-8";
+    # Gnome configurations using home manager
+    gtk.theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome.gnome-themes-extra;
+      gtk3.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
       };
-      "org/cinnamon/desktop/applications/terminal" = {
-        exec = "konsole";
+      gtk4.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
       };
-      # Home Manager will now automatically populate 'enabled-extensions'
-      # You can keep this block if you need to manage disabled ones.
-      "org/gnome/shell" = {
-        disabled-extensions = [
-          "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
-          "status-icons@gnome-shell-extensions.gcampax.github.com"
-          "tilingshell@ferrarodomenico.com"
-          "apps-menu@gnome-shell-extensions.gcampax.github.com"
-          "auto-move-windows@gnome-shell-extensions.gcampax.github.com"
-          "BingWallpaper@sonichy"
-          "window-list@gnome-shell-extensions.gcampax.github.com"
-        ];
+    };
+
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "inode/directory" = "nemo.desktop";
+        "application/x-gnome-saved-search" = [ "nemo.desktop" ];
       };
-      "org/gnome/desktop/interface" = {
-        color-scheme = "prefer-dark";
-        gtk-theme = "Adwaita-dark";
+    };
+    xdg.desktopEntries.nemo = {
+      name = "Nemo";
+      exec = "env GTK_THEME=Adwaita:dark ${pkgs.nemo-with-extensions}/bin/nemo %U";
+      mimeType = [
+        "inode/directory"
+        "application/x-gnome-saved-search"
+      ];
+      icon = "nemo";
+    };
+    xdg.desktopEntries."org.kde.kcalc" = {
+      name = "KCalc";
+      exec = "env QT_STYLE_OVERRIDE=adwaita kcalc";
+      icon = "kcalc";
+      comment = "Scientific Calculator";
+      categories = [
+        "Utility"
+        "Calculator"
+      ];
+    };
+
+    # We can now remove the hardcoded 'enabled-extensions' list
+    dconf = {
+      enable = true;
+      settings = {
+        "system/locale" = {
+          region = osConfig.i18n.defaultLocale or "en_GB.UTF-8";
+        };
+        "org/cinnamon/desktop/applications/terminal" = {
+          exec = "konsole";
+        };
+        # Home Manager will now automatically populate 'enabled-extensions'
+        # You can keep this block if you need to manage disabled ones.
+        "org/gnome/shell" = {
+          disabled-extensions = [
+            "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
+            "status-icons@gnome-shell-extensions.gcampax.github.com"
+            "tilingshell@ferrarodomenico.com"
+            "apps-menu@gnome-shell-extensions.gcampax.github.com"
+            "auto-move-windows@gnome-shell-extensions.gcampax.github.com"
+            "BingWallpaper@sonichy"
+            "window-list@gnome-shell-extensions.gcampax.github.com"
+          ];
+        };
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+          gtk-theme = "Adwaita-dark";
+        };
       };
     };
   };
